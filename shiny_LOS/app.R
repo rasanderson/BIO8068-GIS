aaf95f136f13707d3c3be2dff6a077d2243150c3
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-03-15 13:38:21 +0000

    Shiny app to demonstrate viewshed added

--- shiny_LOS/app.R
+++ shiny_LOS/app.R
@@ -1,85 +1,55 @@
-# #
-# # This is a Shiny web application. You can run the application by clicking
-# # the 'Run App' button above.
-# #
-# # Find out more about building applications with Shiny here:
-# #
-# #    http://shiny.rstudio.com/
-# #
-# 
-# library(shiny)
-# 
-# # Define UI for application that draws a histogram
-# ui <- fluidPage(
-# 
-#     # Application title
-#     titlePanel("Old Faithful Geyser Data"),
-# 
-#     # Sidebar with a slider input for number of bins 
-#     sidebarLayout(
-#         sidebarPanel(
-#             sliderInput("bins",
-#                         "Number of bins:",
-#                         min = 1,
-#                         max = 50,
-#                         value = 30)
-#         ),
-# 
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#            plotOutput("distPlot")
-#         )
-#     )
-# )
-# 
-# # Define server logic required to draw a histogram
-# server <- function(input, output) {
-# 
-#     output$distPlot <- renderPlot({
-#         # generate bins based on input$bins from ui.R
-#         x    <- faithful[, 2]
-#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-# 
-#         # draw the histogram with the specified number of bins
-#         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-#     })
-# }
-
+# Simple demonstration of interactive viewshed calculation
 library(shiny)
+library(sf)
 library(raster)
 library(leaflet)
 library(leafem)
+source("LOS.R")
 
 pan50m <- raster("www/pan50m.tif")
+pan500m <- aggregate(pan50m, fact=5) # aggregate to 500m grid for speed
 ll_crs <- CRS("+init=epsg:4326")
 pan50m_ll <- projectRaster(pan50m, crs=ll_crs)
 
 ui <- fluidPage(
-    leafletOutput(outputId = "pan50m_ll_map")
+    leafletOutput(outputId = "map")
 )
     
 server <- function(input, output, session){
-    output$pan50m_ll_map <- renderLeaflet({
-        leaflet() %>% 
-        addTiles(group = "OSM (default)") %>%
-            addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
-            addRasterImage(pan50m_ll, colors=terrain.colors(25), group = "Elevation North Pennines") %>%
-            addLayersControl(
-                baseGroups = c("OSM (default)", "Satellite"),
-                overlayGroups = "Elevation North Pennines",
-                options = layersControlOptions(collapsed = FALSE)
-            )
+    # Display basemap of terrain
+    output$map <- renderLeaflet({
+        leaflet() %>%
+            setView(lng = -2, lat=53.75, zoom=11) %>%
+            addRasterImage(pan50m_ll, colors=terrain.colors(25))
     })
     
-    observe({
-        coord <- input$pan50m_ll_map_click
-        lng <- coord["lng"][[1]]
-        lat <- coord["lat"][[1]]
-        cat(file=stderr(), lat, " ", lng, "\n")
-    })
-    
-}
+    # Detect a click event and grab coordinates for viewshed
+    observeEvent(input$map_click, {
+        coord <- input$map_click
+        lng <- coord$lng 
+        lat <- coord$lat
 
+        # Create a sf points feature in latitude longitude 4326, and project back to OS 2770
+        # The if(length(c(lng,lat))) needed as when Shiny starts there is no data here
+        # so would otherwise give an error. On clicking the map, the lng and lat recorded
+        if(length(c(lng,lat))==2){
+            turbine_pt_ll <- data.frame(lat = lat, lng = lng) %>% 
+                st_as_sf(coords = c("lng", "lat")) %>% 
+                st_set_crs(4326)
+            turbine_pt_os <- st_transform(turbine_pt_ll, crs=27700)
+            turbine_pt_os <- st_geometry(turbine_pt_os)[[1]] # Only want geometry
+
+            # Calculate 5km viewshed and reproject back to lat-lon
+            viewshed_5km_os <- viewshed(dem=pan500m, windfarm=turbine_pt_os,
+                                        h1=1.5, h2=50, radius=5000)
+            viewshed_5km_ll <- projectRaster(viewshed_5km_os, crs=ll_crs)
+
+            # Add to existing map            
+            leafletProxy("map") %>% 
+                addRasterImage(viewshed_5km_ll, color="red")
+        }
+    })
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
